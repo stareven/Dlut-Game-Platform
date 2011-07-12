ccf61b9193a74bc2517b25cb6fed3eaa9bd916bc
lexdene <lexdene@sohu.com>
2011-07-12 17:15:59 +0800

    update protocol of snake

--- AllBuild.sh
+++ AllBuild.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 find . -path '*-build-desktop' -exec make -C {} \;
+printf "build finish . press any key to escape:"
 read -N 1
--- SnakeCommon/jsnakeglobal.h
+++ SnakeCommon/jsnakeglobal.h
@@ -13,6 +13,13 @@ namespace Snake{
 		QString m_roomName;
 		QSet<JID> m_players;
 	};
+	class JRoomId2Num
+	{
+	public:
+		JRoomId2Num(const JRoom& room);
+		JID getIdByNum(int num);
+		int getNumById(JID Id);
+	};
 }
 QDataStream& operator>>(QDataStream& ,Snake::JRoom&);
 QDataStream& operator<<(QDataStream& ,const Snake::JRoom&);
@@ -49,6 +56,29 @@ namespace SnakeProtocol{
 	const JProtocol SP_Userlist = 0x4000;
 		// C -> S : SP_Userlist ,
 		// S -> C : SP_Userlist , <JID>roomid , <QList<JID> >userlist ,
-	const JProtocol SP_GameAct = 0x5000;
+	const JProtocol SP_GameInfo = 0x5000;
+	const JProtocol SP_GI_UseridList = ( SP_GameInfo | 0x0100 );
+		// C -> S : SP_GI_UseridList
+		// S -> C : SP_GI_UseridList , <QList<JID> >useridlist
+	const JProtocol SP_GI_Ready = ( SP_GameInfo | 0x0200 );
+		// C -> S : SP_GI_Ready , <bool>ready
+			// true : ready ; false : cancel ready
+		// C -> S : SP_GI_Ready , <bool>ready , <JID>userid
+	const JProtocol SP_GameAct = 0x6000;
+	const JProtocol SP_GA_CountDown = ( SP_GameAct | 0x0100 );
+		// S -> C : SP_GA_CountDown , <int>n
+	const JProtocol SP_GA_GetCommand = ( SP_GameAct | 0x0200 );
+		// S -> C : SP_GA_GetCommand
+	const JProtocol SP_GA_Turn = ( SP_GameAct | 0x0300 );
+		// C -> S : SP_GA_Turn , <JSnake::EDire>dire
+		// S -> C : SP_GA_Turn , <JSnake::EDire>dire , <JID>userid
+	const JProtocol SP_GA_Collision = ( SP_GameAct | 0x0400 );
+		// S -> C : SP_GA_Collision , <JID>userid
+	const JProtocol SP_GA_CreateBean = ( SP_GameAct | 0x0500 );
+		// S -> C : SP_GA_CreateBean , <QPoint>pt
+	const JProtocol SP_GA_IncreaseScore = ( SP_GameAct | 0x0600 );
+		// S -> C : SP_GA_IncreaseScore , <JID>userid
+	const JProtocol SP_GA_MoveOn = ( SP_GameAct | 0x0700 );
+		// S -> C : SP_GA_MoveOn , <JID>userid
 }
 #endif // JSNAKEGLOBAL_H
